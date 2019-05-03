a03e85900b9d67548dbdda3a1e7fd822c32afc47
Thomas <honiballthomas@gmail.com>
2019-05-03 10:50:06 +0200

    Implemented Database Unit Testing
    Basic unit testing present for database

--- Code/library/db.js
+++ Code/library/db.js
@@ -2,38 +2,63 @@ import { SQLite } from 'expo';
 
 export default class dbconnection{
 	database: null;
+	created: false;
+	filled: false;
+	open: false;
 	constructor(props)
 	{
-		database = SQLite.openDatabase("indoors");
+		this.database = SQLite.openDatabase("indoors");
+		this.open = true;
+		//console.log("opened");
 		this.buildTable();
 		this.fillTable();
-		
+		//console.log(this.created);
+	}
+
+	checkStatus(property = "all"){
+		if(property == "all")
+		{
+			return [this.open, this.created, this.filled];
+		}else if(property == "open")
+		{
+			return this.open;
+		}else if(property == "created")
+		{
+			return this.created;
+		}else if(property == "filled")
+		{
+			return this.filled;
+		}else
+		{
+			return false;
+		}
+
 	}
 
 	buildTable (){
-		database.transaction(tx => {
+		this.database.transaction(tx => {
 			tx.executeSql(
 				"CREATE TABLE IF NOT EXISTS tbl_items('barcode_id' INTEGER PRIMARY KEY AUTOINCREMENT,'name' TEXT(50) )",
 				[],
-				(tx, success)=>{alert("created");}, 
-				(tx, error)=>{alert("failed to create " + error)}
+				(tx, success)=>{console.log("DB created");}, 
+				(tx, error)=>{console.log("DB create error " + error);}
 			);
 		});
 	}
 	fillTable (){
-		database.transaction(tx => {
+		this.database.transaction(tx => {
 			tx.executeSql(
 				"INSERT INTO tbl_items(name)VALUES('Shoes'), ('Hamburger'), ('Cellphone')",
 				[],
-				(tx, success)=>{alert("filled")}, 
-				(tx, error)=>{alert("failed to fill " + error)}
+				(tx, success)=>{console.log("DB Filled");}, 
+				(tx, error)=>{console.log("DB fill error " + error);}
 			);
 		});
 	}
 
 	checkBarcode(barcode) {
 		alert(barcode)
-		database.transaction(tx => {
+		this.database.transaction(tx => {
 			tx.executeSql(
 				"SELECT * FROM tbl_items WHERE barcode_id = ?",
 				[barcode],

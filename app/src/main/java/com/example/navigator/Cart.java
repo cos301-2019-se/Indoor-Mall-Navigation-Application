d7c314e58548deba86e9e5bc6e9218cc5524012f
Mpho Mashaba <36999004+MphoMashaba@users.noreply.github.com>
2019-07-18 23:48:35 +0200

    Cart list

--- app/src/main/java/com/example/navigator/Cart.java
+++ app/src/main/java/com/example/navigator/Cart.java
@@ -3,27 +3,87 @@ package com.example.navigator;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static android.app.Activity.DEFAULT_KEYS_DIALER;
+import static com.example.navigator.MainActivity.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class Cart extends Fragment {
 
+    Button  fetch;
+    private FirebaseAuth firebaseAuth;
+    TextView demoValue;
 
+    DatabaseReference rootRef,demoRef;
     public Cart() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false);
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        fetch = (Button) view.findViewById(R.id.fetch);
+        demoValue = (TextView) view.findViewById(R.id.tvValue);
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        //database reference pointing to Product node
+        demoRef = rootRef.child("Product");
+
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        List<Object> list = new ArrayList<>();
+                        Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                        for(int i = 0; i < map.size(); i++){
+                            list.add(map);
+                            i++;
+                        }
+
+                        Log.d(TAG, "Barcodes: " + list);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+
+                });
+            }
+
+
+        });
+        return view;
     }
+
 
 }

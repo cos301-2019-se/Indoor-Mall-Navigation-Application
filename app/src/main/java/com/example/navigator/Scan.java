677b6c601582012f69c643009286b9ecea78a40a
Bandile Dlamini <u14402425@tuks.co.za>
2019-10-10 17:04:09 +0200

    Updated Create Invoce Function

--- app/src/main/java/com/example/navigator/Scan.java
+++ app/src/main/java/com/example/navigator/Scan.java
@@ -20,26 +20,16 @@
  *
  */
 package com.example.navigator;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,18 +41,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.navigator.utils.Installation;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -77,17 +62,14 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import org.w3c.dom.Text;
-
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
 import adapters.ComparePriceDialog;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -198,7 +180,7 @@ public class Scan extends Fragment {
       generatePDF.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-          createPdf2("123456789", "11-09-2019", "Bandile Dlamini");
+          createClientInvoice(getDummyProducts(),"123456789", "11-09-2019", "Bandile Dlamini");
         }
       });
 
@@ -510,7 +492,7 @@ public class Scan extends Fragment {
     }
 
 
-  private void createPdf2(String invoceNum, String date, String customerName){
+  private void createClientInvoice(ArrayList<Product> products, String invoceNum, String date, String customerName){
 
     /*Paragraph p = new Paragraph();
     Chunk c = new Chunk();
@@ -533,17 +515,20 @@ public class Scan extends Fragment {
     for (int j=0;j<cells.length;j++){
       cells[j].setBackgroundColor(BaseColor.GRAY);
     }
-    for (int i=1;i<=10;i++){
-      table.addCell("Description:"+i);
-      table.addCell("Shop:"+i);
-      table.addCell("Cost:"+i);
-      table.addCell("Qty:"+i);
-      table.addCell("Amount:"+i);
 
+    for (int i = 0; i < products.size(); i++){
+      table.addCell(products.get(i).getName());
+      table.addCell("Shop:"+i);
+      table.addCell("R" + products.get(i).price);
+      table.addCell("" + products.get(i).getQuantity());
+      double no=products.get(i).getPrice() * products.get(i).getQuantity();
+      DecimalFormat dec = new DecimalFormat("#0.00");
+      table.addCell("R" + dec.format(no));
     }
+
     try {
       String directory_path = Environment.getExternalStorageDirectory().getPath() + "/Indoor Mall Navigator/";
-      writer = PdfWriter.getInstance(document, new FileOutputStream(directory_path + "sample3.pdf"));
+      writer = PdfWriter.getInstance(document, new FileOutputStream(directory_path + "Invoice" + getDateTime() + ".pdf"));
       document.open();
 
       Font H1=new Font(Font.FontFamily.TIMES_ROMAN,30.0f, Font.BOLD, BaseColor.BLACK);
@@ -586,9 +571,10 @@ public class Scan extends Fragment {
       p = new Paragraph("-");
       document.add(p);
 
-
       absoluteText("---------------------------------------------------------", 360, 40);
-      absoluteText("Total: ", 400, 20);
+      Double total = 0.00;
+      DecimalFormat dec = new DecimalFormat("#0.00");
+      absoluteText("Total: R" + dec.format(total), 400, 20);
 
       document.add(table);
       document.close();
@@ -601,6 +587,117 @@ public class Scan extends Fragment {
 
   }
 
+  private void createMerchantsReceipt(ArrayList<Product> products, String invoceNum, String date, String customerName, String merchantName){
+
+    /*Paragraph p = new Paragraph();
+    Chunk c = new Chunk();
+    Image i = Image.getInstance("resources/images/fox.bmp");
+    c = new Chunk(i, 0, -24);
+    p.add(c);*/
+
+
+    Document document = new Document();
+    PdfPTable table = new PdfPTable(new float[] { 5, 1, 1, 1 });
+    table.setWidthPercentage(100);
+    table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+    table.addCell("Description");
+    table.addCell("Unit Cost");
+    table.addCell("Qty");
+    table.addCell("Amount");
+    table.setHeaderRows(1);
+    PdfPCell[] cells = table.getRow(0).getCells();
+    for (int j=0;j<cells.length;j++){
+      cells[j].setBackgroundColor(BaseColor.GRAY);
+    }
+    for (int i=1;i<=10;i++){
+      table.addCell("Description:"+i);
+      table.addCell("Cost:"+i);
+      table.addCell("Qty:"+i);
+      table.addCell("Amount:"+i);
+
+    }
+    try {
+      String directory_path = Environment.getExternalStorageDirectory().getPath() + "/Indoor Mall Navigator/";
+      writer = PdfWriter.getInstance(document, new FileOutputStream(directory_path + merchantName + "Receipt" + getDateTime() + ".pdf"));
+      document.open();
+
+      Font H1=new Font(Font.FontFamily.TIMES_ROMAN,30.0f, Font.BOLD, BaseColor.BLACK);
+      Paragraph p = new Paragraph();
+
+      Drawable d = getResources().getDrawable(R.drawable.logo3);
+      BitmapDrawable bitDw = ((BitmapDrawable) d);
+      Bitmap bmp = bitDw.getBitmap();
+      ByteArrayOutputStream stream = new ByteArrayOutputStream();
+      bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+      Image image = Image.getInstance(stream.toByteArray());
+      Chunk c = new Chunk(image, 0, -90);
+      p.add(c);
+      document.add(p);
+
+      p = new Paragraph(merchantName+"RECEIPT", H1);
+      p.setAlignment(Paragraph.ALIGN_RIGHT);
+      document.add(p);
+      p = new Paragraph("Indoor Mall Navigator");
+      p.setAlignment(Paragraph.ALIGN_RIGHT);
+      document.add(p);
+      p = new Paragraph("Invoice No. " + invoceNum);
+      p.setAlignment(Paragraph.ALIGN_RIGHT);
+      document.add(p);
+      p = new Paragraph("Date: " + date);
+      p.setAlignment(Paragraph.ALIGN_RIGHT);
+      document.add(p);
+      p = new Paragraph("Billed To: " + customerName);
+      p.setAlignment(Paragraph.ALIGN_LEFT);
+      document.add(p);
+      p = new Paragraph("Address Line 1");
+      p.setAlignment(Paragraph.ALIGN_LEFT);
+      document.add(p);
+      p = new Paragraph("Address Line 2");
+      p.setAlignment(Paragraph.ALIGN_LEFT);
+      document.add(p);
+      p = new Paragraph("Address Line 3");
+      p.setAlignment(Paragraph.ALIGN_LEFT);
+      document.add(p);
+      p = new Paragraph("-");
+      document.add(p);
+
+      absoluteText("---------------------------------------------------------", 360, 40);
+      Double total = 0.00;
+      absoluteText("Total: R" + total, 400, 20);
+
+      document.add(table);
+      document.close();
+      Toast.makeText(getContext(), "Done", Toast.LENGTH_LONG).show();
+    }
+    catch (Exception e){
+      e.printStackTrace();
+      Toast.makeText(getContext(), "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
+    }
+
+  }
+
+  private  final static String getDateTime()
+  {
+    DateFormat df = new SimpleDateFormat("yyMMddhhmm");
+    return df.format(new Date());
+  }
+
+  private ArrayList<Product> getDummyProducts()
+  {
+    ArrayList<Product> products = new ArrayList<>();
+
+    products.add(new Product("5060466519077","Power Play",19.99,1));
+    products.add(new Product("8718114642871","Vaseline Lip T",23.99,2));
+    products.add(new Product("6009635830536","Manuscript Book",10.99,5));
+    products.add(new Product("6009695584912","Bioplus",4.99,4));
+    products.add(new Product("6003326009584","Flying Fish Pressed Lemmon",15.99,1));
+    products.add(new Product("6009690380038","Oasis Still 500ml",9.99,8));
+    products.add(new Product("60018939","Vaseline Blueseal",23.99,5));
+    products.add(new Product("6001120602871","Jungle Bar",10.99,2));
+    products.add(new Product("6001120624972","Sour Jelly Beans",22.99,2));
+
+    return products;
+  }
 
   private void absoluteText(String text, int x, int y) {
     try {
